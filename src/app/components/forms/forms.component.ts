e5f9906fa40d3a374fcf44268a69da7823532650
romiltondev <romiltoncosta73@gmail.com>
2024-08-12 09:22:44 -0300

    feat: create get unit service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-
+import { Location } from '../../types/location.interface';
+import { GetUnitsService } from '../../services/get-units.service';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent implements OnInit {
-
-  results=[];
+  @Output() submitEvent= new EventEmitter();
+  results:Location[]=[];
+  filteredResults: Location[] = [];
   formGroup!:FormGroup;
 
-  constructor(private formBuider:FormBuilder){}
+  constructor(
+    private formBuider:FormBuilder,
+    private unitService:GetUnitsService,
+    private filterUnitsService:FilterUnitsService
+  ){}
+  
   ngOnInit():void{
     this.formGroup=this.formBuider.group({
       hour:'',
@@ -20,7 +27,7 @@ export class FormsComponent implements OnInit {
   }
 
   onSubmit():void{
-    
+
   }
 
 }
