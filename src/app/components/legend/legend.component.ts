9a5e5f1d9b8e2a24652f32820732cd977bf8dca6
romiltondev <romiltoncosta73@gmail.com>
2024-08-12 10:26:58 -0300

    feat: add legen and footer

---
+++ src/app/components/legend/legend.component.ts
@@ -0,0 +1,13 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-legend',
+  templateUrl: './legend.component.html',
+  styleUrl: './legend.component.scss'
+})
+export class LegendComponent implements OnInit{
+ constructor(){}
+ ngOnInit(): void {
+   
+ }
+}
